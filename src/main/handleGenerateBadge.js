8751c9ceab83c398e8f680bdcccff16d14f1d92c
Savva Mikhalevski <smikhalevski@gmail.com>
2017-02-25 06:20:42 +0300

    Fixed #5

--- src/main/handleGenerateBadge.js
+++ src/main/handleGenerateBadge.js
@@ -4,10 +4,15 @@ export function handleGenerateBadge(request, response, next) {
   const {badgeSpec} = request.params;
   const [,badgeId] = badgeSpec.match(/^(.+)\.([^.]+)$/);
 
-  renderBadge(badgeId, request.query)
+  const {maxAge = 3600, ...query} = request.query;
+
+  renderBadge(badgeId, query)
       .then(source => {
         response
-            .header('Content-Type', 'image/svg+xml')
+            .header({
+              'Content-Type': 'image/svg+xml',
+              'Cache-Control': `public, max-age=${maxAge}`
+            })
             .send(source);
         next();
       })
