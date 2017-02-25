c0912a29cc75c431309f4aae854816491b42e73f
Savva Mikhalevski <smikhalevski@gmail.com>
2017-02-25 06:09:48 +0300

    Added border for white Github label badges

--- src/main/getBadgeById.js
+++ src/main/getBadgeById.js
@@ -1,11 +1,14 @@
 const GITHUB_LABEL = `
   <svg xmlns="http://www.w3.org/2000/svg"
        width={Math.round(widthOfString(name)) + 6}
-       height="18">
+       height="20">
     <rect fill={'#' + color}
+          stroke={color == 'fff' ? '#eee' : null}
+          x="1"
+          y="1"
           rx="2"
           ry="2"
-          width="100%"
+          width={Math.round(widthOfString(name)) + 4}
           height="18"/>
     <text fill={Color('#' + color).luminosity() > .4 ? '#000' : '#fff'}
           x="50%"
@@ -19,9 +22,10 @@ const GITHUB_LABEL = `
   </svg>
 `;
 
-export function getBadgeById(id) {
-  if (id == 'github-label') {
-    return GITHUB_LABEL;
+export function getBadgeById(badgeId) {
+  switch (badgeId) {
+    case 'github-label':
+      return GITHUB_LABEL;
   }
   throw new Error(`Template ${id} could not be found`);
 }
